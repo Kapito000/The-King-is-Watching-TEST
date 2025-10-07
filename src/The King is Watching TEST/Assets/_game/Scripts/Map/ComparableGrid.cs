c47d030ba784bb8da85fa7cc932834981ab5d0b0
Egor <rkrivzha@yandex.ru>
2025-10-07 15:55:05 +0300

    Maps code.

---
+++ src/The King is Watching TEST/Assets/_game/Scripts/Map/ComparableGrid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Map
+{
+	public class ComparableGrid<T> : Grid<T> where T : IComparable
+	{
+		public ComparableGrid(int xSize, int ySize) : base(xSize, ySize)
+		{ }
+
+		protected override bool IsEquals(T a, T b) =>
+			a.CompareTo(b) == 0;
+	}
+}
