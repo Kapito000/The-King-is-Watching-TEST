813e9f8110b5e4bc12cbdbb6d9f8386c28d0d759
Egor <rkrivzha@yandex.ru>
2025-10-08 15:36:44 +0300

    Extensions.

---
+++ src/The King is Watching TEST/Assets/_game/Scripts/Extensions/Vector2Extensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+	public static class Vector2Extensions
+	{
+		public static Vector3 AsVector3(this Vector2Int v) =>
+			new(v.x, v.y, 0);
+
+		public static Vector2Int Rotate90(this Vector2Int v) =>
+			new(v.y, -v.x);
+
+		public static Vector2Int Rotate180(this Vector2Int v) =>
+			-v;
+
+		public static Vector2Int Rotate270(this Vector2Int v) =>
+			new(-v.y, v.x);
+	}
+}
---
+++ src/The King is Watching TEST/Assets/_game/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+	public static class Vector3Extensions
+	{
+		public static Vector2Int AsVector2Int(this Vector3 v) =>
+			new Vector2Int((int)v.x, (int)v.y);
+	}
+}
